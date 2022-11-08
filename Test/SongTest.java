c1a5895c2554f3dd06309a04b37387913eec761e
Maddison <mtranchau1@gmail.com>
2022-11-07 19:01:19 -0800

    11/7/22: 7:00pm - added passing tests for name getters and setters, (passing and failing) tests for length getters and setters.  next steps: add regex to length setter to pass tests.

--- Test/SongTest.java
+++ Test/SongTest.java
@@ -4,10 +4,80 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SongTest
 {
+    // tests for NAME getters and setters
+    // when song name is traditional (capitals, lowercase, and spaces)
     @Test
     void getNameTSLoveStoryPassed() {
         Song loveStory = new Song("Love Story");
         loveStory.setName("Love Story");
         assertTrue(loveStory.getName().matches("Love Story"));
     }
+
+    // when song name has special characters - ":)"
+    @Test
+    void getNameGlitterPartyPassed() {
+        Song smile = new Song(":)");
+        smile.setName(":)");
+        assertTrue(smile.getName().matches(":\\)"));
+    }
+
+    // when song name has capitals, lowercase, spaces, and commas
+    @Test
+    void getNameSACWPassed() {
+        Song SACW = new Song("So Alright, Cool, Whatever");
+        SACW.setName("So Alright, Cool, Whatever");
+        assertTrue(SACW.getName().matches("So Alright, Cool, Whatever"));
+    }
+
+    // when song name is in kanji
+    @Test
+    void getNameSunsetRoadPassed() {
+        Song sunsetRoad = new Song("サンセット・ロード");
+        sunsetRoad.setName("サンセット・ロード");
+        assertTrue(sunsetRoad.getName().matches("サンセット・ロード"));
+    }
+
+
+    // tests for LENGTH getters and setters
+    // when length is normal (number:number number)
+    @Test
+    void getLengthSunsetRoadPassed() {
+        Song sunsetRoad = new Song("サンセット・ロード", "4:18");
+        sunsetRoad.setLength("4:18");
+        assertTrue(sunsetRoad.getLength().matches("4:18"));
+    }
+
+    // when length is normal, but first num is two digits (two-digit number:number number)
+    @Test
+    void getLengthOldiePassed() {
+        Song oldie = new Song("Oldie", "10:36");
+        oldie.setLength("10:36");
+        assertTrue(oldie.getLength().matches("10:36"));
+    }
+
+    // when length has too many digits
+    @Test
+    void getLengthDSSMFailed() {
+        Song DSSM = new Song("Dream Sweet in Sea Major", "7:00684512");
+        DSSM.setLength("7:00684512");
+        assertTrue(DSSM.getLength().matches("A song can't be that long!"));
+    }
+
+    // when length is empty
+    @Test
+    void getLengthECGMFailed() {
+        Song ECGM = new Song("Existential Crisis in G Major", "");
+        ECGM.setLength("");
+        assertTrue(ECGM.getLength().matches("A song can't be that long!"));
+    }
+
+    // when length is negative
+    @Test
+    void getLengthSITYWTDFailed() {
+        Song SITYWTD = new Song("SWEET \\/ I THOUGHT YOU WANTED TO DANCE", "-9:48");
+        SITYWTD.setLength("-9:48");
+        assertTrue(SITYWTD.getLength().matches("A song can't be that long!"));
+    }
+
+
 } // end tests
--- src/Song.java
+++ src/Song.java
@@ -9,24 +9,41 @@ public class Song
     public Song (String name)
     {
         setName(name);
-    }
+        setLength(length);
+    } // end Song constructor for name only
+
+    public Song (String name, String length)
+    {
+        setName(name);
+        setLength(length);
+    } // end Song constructor for name and length
 
     public void setName (String name)
     {
-        if (!name.matches("[A-Za-z]"))
+        if (!name.matches(".*"))
         {
             // System.out.println("NOPE!");
-            this.name = "Wrong format";
+            this.name = "Wrong format!";
         }
         else
         {
             this.name = name;
         }
-    }
+    } // end setName
+
+    public void setLength(String length)
+    {
+        this.length = length;
+    } // end setLength
 
     public String getName ()
     {
         return name;
-    }
+    } // end getName
 
-}
+    public String getLength()
+    {
+        return length;
+    } // end getLength
+
+} // end Song class
